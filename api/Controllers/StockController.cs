0d50de47d750d2dafe0e7e787b90de480abee659
Arianna Foo <fooar@sheridancollege.ca>
2025-12-29 22:12:35 -0500

    Stock DTO + StockMapper created

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using api.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Mvc;
+using api.Mappers;
 
 namespace api.Controllers
 {
@@ -23,7 +24,8 @@ namespace api.Controllers
         [HttpGet]
         public IActionResult GetAll()
         {
-            var stocks  = _context.Stock.ToList();
+            var stocks = _context.Stock.ToList()
+            .Select(s => s.ToStockDto());
             return Ok(stocks);
         }
 
@@ -36,7 +38,7 @@ namespace api.Controllers
             {
                 return NotFound();
             }
-            return Ok(stock);
+            return Ok(stock.ToStockDto());
         }
     }
 }
