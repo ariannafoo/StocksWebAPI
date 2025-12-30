3495b4e8d83383a4b85d2712aa3c94662981e86b
Arianna Foo <fooar@sheridancollege.ca>
2025-12-29 19:24:44 -0500

    Stock controller w/ GET endpoints implemented

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -2,11 +2,41 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
 
 namespace api.Controllers
 {
-    public class StockController
+    [Microsoft.AspNetCore.Mvc.Route("api/Stock")]
+    [ApiController]
+    public class StockController : ControllerBase
     {
-        
+        // Making it read only
+        // here to get things in and out of the database
+        private readonly ApplicationDbContext _context;
+        public StockController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var stocks  = _context.Stock.ToList();
+            return Ok(stocks);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById([FromRoute] int id)
+        {
+            var stock = _context.Stock.Find(id);
+
+            if (stock == null)
+            {
+                return NotFound();
+            }
+            return Ok(stock);
+        }
     }
 }
